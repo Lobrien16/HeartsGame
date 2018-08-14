deca6943d58cf2b52c51231d4566e2c737349c31
NamdiMaple31 <namdimaple@gmail.com>
2018-08-14 06:52:20 -0700

    message

--- main.js
+++ main.js
@@ -9,99 +9,85 @@ const ness = {
 const paula = {
     width : 50,
     height : 50,
-    x : 0,
-    y : 0,
+    x : 200,
+    y : 300,
     speed: 3
 }
 
 const jeff = {
     width : 50,
     height : 50,
-    x : 0,
-    y : 0,
+    x : 40,
+    y : 60,
     speed: 3
 }
 
 const poo = {
     width : 50,
     height : 50,
-    x : 0,
-    y : 0,
+    x : 550,
+    y : 120,
     speed: 3
 }
 
-const npc = [];
-
+const npcs = [paula, jeff, poo];
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     rectMode(CENTER);
     ness.x = width / 2;
     ness.y = height / 2;
-    paula.x = width / 2;
-    paula.y = width / 2;
-    jeff.x = width / 2;
-    jeff.y = width / 2;
-    poo.x = width / 2;
-    poo.y = width / 2;
-    for (let i = 0; i < 6; i++) {
-        const array = [];
-
-        for (let j = 0; j < 3; j++) {
-            array.push(true);
-        }
-
-        npc.push(array);
-    }
 }
 
 function draw() {
     background("yellow");
     fill("grey");
     drawSprites();
-    handleMovement();
-    rect(50, 50, 50, 50);
-    for(let i = 0; i < 6; i++) {
-        for(let j = 0; j < 3; j++) {
-            fill("green");
-            const x = (i * 100) + 50;
-            const y = (j * 100) + 50;
+    const collided = detectCollision();
 
-            if (npc[i][j] === true) {
-                rect(x, y, 50, 50);
-
-                const distance = dist(bulletX, bulletY, x, y)
-
-                if (distance <= (50/2) + (25/2)){
-                    npc[i][j] = false;
-                    bulletY = y2
-                    bulletX = x2
-                    bulletSpeed = 0
-                }
-             }
- }
+    if (!collided) {
+        handleMovement();  
+    } else {
+        text('You collided!', width / 2, height / 2);
     }
 }
 
-// function handleMovement() {
-//     if(keyIsDown(LEFT_ARROW)) {
-//         ness.x -= ness.speed;
-//     } 
-//     if(keyIsDown(RIGHT_ARROW)) {
-//         ness.x += ness.speed;
-//     }
-//     if(keyIsDown(UP_ARROW)) {
-//         ness.y -= ness.speed;
-//     }
-//     if(keyIsDown(DOWN_ARROW)) {
-//         ness.y += ness.speed;
-//     }
-   
+function detectCollision() {
+    for (const npc of npcs) {
+        const distance = dist(ness.x, ness.y, npc.x, npc.y);
 
-// }
+        if (distance <= (ness.width / 2) + npc.width / 2) {
+            return true;
+        }
+    }
 
-//function drawSprites() {
-    // draw ness
-   // rect(ness.x, ness.y, ness.width, ness.height);
-   // }
+    return false;
+}
+
+function handleMovement() {
+    if(keyIsDown(LEFT_ARROW)) {
+        ness.x -= ness.speed;
+    } 
+
+    if(keyIsDown(RIGHT_ARROW)) {
+        ness.x += ness.speed;
+    }
+
+    if(keyIsDown(UP_ARROW)) {
+        ness.y -= ness.speed;
+    }
+
+    if(keyIsDown(DOWN_ARROW)) {
+        ness.y += ness.speed;
+    }
+}
+
+function drawSprites() {
+    //draw ness
+   rect(ness.x, ness.y, ness.width, ness.height);
+
+   for (const npc of npcs) {
+       rect(npc.x, npc.y, npc.width, npc.height);
+   }
+}
 
